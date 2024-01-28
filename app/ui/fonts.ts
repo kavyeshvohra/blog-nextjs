635987675196e832c8823fbebc0d287ca62e95d8
kavyeshvohra <vohrakavyesh@gmail.com>
2024-01-28 16:35:18 +0530

    Next.js app commited to domain

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Inter_Tight} from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+export const inter = Inter_Tight({subsets:['latin']});
+export const luistana = Lusitana({weight:"400",subsets:['latin']});
